b804bf9d988056480ea825b8ec74863ac6b679f6
Murtaza Uddin Fatmi <104943361+janicoder@users.noreply.github.com>
2022-09-08 22:58:10 +0500

    stackcode test

--- music.js
+++ music.js
@@ -6,3 +6,20 @@ for (var i = 0; i < songs.length; i++) {
     } 
 }
 songs[0].play();
+
+var c = 0;
+var songs = ["./rss/music/Perfect.mp3", "./rss/music/DuskTillDawn.mp3", "./rss/music/CheapThrills.mp3", "./rss/music/AllOfMe.mp3", "./rss/music/Snap.mp3", "./rss/music/LoveNwantiti.mp3", "./rss/music/LoveMeLikeYouDo.mp3", "./rss/music/AgarTumSathHo.mp3"];
+
+var a = document.getElementById("au");
+a.addEventListener('ended', function(){
+
+document.getElementById("au").src=songs[c]; 
+a.load();  
+console.log(c); 
+c++;
+
+if(c>=songs.length){
+    c=0;
+}
+
+});
