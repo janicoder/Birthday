86c06c0de7b6c9e685f13ef706c12a422f18c154
Murtaza Uddin Fatmi <104943361+janicoder@users.noreply.github.com>
2022-09-08 22:59:11 +0500

    stack test 3

--- music.js
+++ music.js
@@ -1,12 +1,3 @@
-var songs = document.getElementsByClassName('song');
-
-for (var i = 0; i < songs.length; i++) {
-    songs[i].onended = function() {
-        songs[i + 1].play();
-    } 
-}
-songs[0].play();
-
 var c = 0;
 var songs = ["./rss/music/DuskTillDawn.mp3", "./rss/music/CheapThrills.mp3", "./rss/music/Snap.mp3", "./rss/music/LoveNwantiti.mp3", "./rss/music/LoveMeLikeYouDo.mp3", "./rss/music/AgarTumSathHo.mp3"];
 
