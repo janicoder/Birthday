d9999ed481ae39db7e22c1e1e86403be1479b75e
Murtaza Uddin Fatmi <104943361+janicoder@users.noreply.github.com>
2022-09-14 03:09:49 +0500

    next back buttons

--- music.js
+++ music.js
@@ -1,6 +1,6 @@
 var c = 0;
-var songs = ["./rss/music/DuskTillDawn.mp3", "./rss/music/CheapThrills.mp3", "./rss/music/Snap.mp3", "./rss/music/LoveNwantiti.mp3", "./rss/music/LoveMeLikeYouDo.mp3", "./rss/music/AgarTumSathHo.mp3"];
-
+var songs = ["./rss/music/Perfect.mp3", "./rss/music/AgarTumSathHo.mp3", "./rss/music/DuskTillDawn.mp3", "./rss/music/CheapThrills.mp3", "./rss/music/Snap.mp3", "./rss/music/LoveNwantiti.mp3", "./rss/music/LoveMeLikeYouDo.mp3", "./rss/music/AgarTumSathHo.mp3"];
+var initial = true;
 var a = document.getElementById("au");
 a.addEventListener('ended', function(){
 
@@ -26,10 +26,47 @@ playPause.addEventListener('click', function(){
         playIcon.style.display = "inline";
         pauseIcon.style.display = "none";
     } else {
-        a.play()
+        if(initial) {
+            a.src = songs[0];
+            initial = false;
+            c++;
+        }
+        a.play();
         pauseIcon.style.display = "inline";
         playIcon.style.display = "none";
     }
+});
+
+var next = document.getElementById("next");
+next.addEventListener('click', function(){
+    document.getElementById("au").src=songs[c]; 
+    a.load();
+    a.play();
+    console.log(c); 
+    c++;
+    
+    if(c>=songs.length){
+        c=0;
+    }
+    pauseIcon.style.display = "inline";
+    playIcon.style.display = "none";
+});
+
+var back = document.getElementById("back");
+back.addEventListener('click', function(){
+    if (c != 0) {
+        c--;
+        c--;
+    document.getElementById("au").src=songs[c];
+    a.load();
+    a.play();
+    console.log(c); 
+    c++;
+    
+    if(c>=songs.length){
+        c=0;
+    }
+    };
 });
 
 //Volume Control
